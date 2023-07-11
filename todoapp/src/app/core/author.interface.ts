4f26ef2b842ac882f9ba8141f1e9cf42f77960c8
Hafdala Mehdi Sidi <hafdalamehdisidi@gmail.com>
2023-07-12 00:45:27 +0200

    I have created the core folder, the shared folder and the task folder, as well as setting up aliases for them and routing. I have also typed the auth module.

---
+++ todoapp/src/app/core/author.interface.ts
@@ -0,0 +1,7 @@
+export interface AuthorInterface {
+    _id:string;
+    name:string;
+    avatar:string;
+    email:string;
+    
+}
---
+++ todoapp/src/app/core/task.inteface.ts
@@ -0,0 +1,8 @@
+import { AuthorInterface } from "./author.interface";
+
+export interface TaskInterface {
+    title:string;
+    description:string;
+    author:AuthorInterface;
+
+}
